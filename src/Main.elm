4177dffb7181188e0596bd300907bd3817d044d6
Hugo Bessa <eu@hugobessa.com.br>
2017-06-09 18:26:08 -0300

    Split Talk and Event schedule items

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,27 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 
+viewTalk =
+    li [ class "pv1" ]
+        [ time [] [ text "9:30" ]
+        , h4 [ class "f3 ma0 normal" ] [ text "Keynote" ]
+        , div [ class "flex items-center" ]
+            [ div [ class "pr1" ]
+                [ img [ class "w1 h1 br-100 bn bg-light-silver" ] []
+                ]
+            , span [ class "lh-solid" ] [ text "Matheus Marsiglio" ]
+            ]
+        ]
+
+
+viewEvent =
+    li [ class "pv1" ]
+        [ time [] [ text "9:30" ]
+        , h4 [ class "f4 ma0 normal" ] [ text "Cadastro" ]
+        , span [ class "" ] [ text "Não esqueça seu RG" ]
+        ]
+
+
 main =
     div []
         [ header [ class "pa3 pt4" ]
@@ -14,21 +35,8 @@ main =
         , section [ class "pa3" ]
             [ h2 [ class "subheadline" ] [ text "Agenda" ]
             , ul [ class "list pa0 ma0" ]
-                [ li [ class "pv1" ]
-                    [ time [] [ text "9:30" ]
-                    , h4 [ class "f4 ma0 normal" ] [ text "Cadastro" ]
-                    , span [ class "" ] [ text "Não esqueça seu RG" ]
-                    ]
-                , li [ class "pv1" ]
-                    [ time [] [ text "9:30" ]
-                    , h4 [ class "f3 ma0 normal" ] [ text "Keynote" ]
-                    , div [ class "flex items-center" ]
-                        [ div [ class "pr1" ]
-                            [ img [ class "w1 h1 br-100 bn bg-light-silver" ] []
-                            ]
-                        , span [ class "lh-solid" ] [ text "Matheus Marsiglio" ]
-                        ]
-                    ]
+                [ viewEvent
+                , viewTalk
                 ]
             ]
         , footer [] []
